a7f0e5309dc2a15b46c27a1a76fbf77484392f70
Luu Duc Dong <luuducdong@Ludo-Macbook-Pro.local>
2022-08-23 14:31:33 +0700

    Add connect description for android side

--- lib/src/components/modal_wallet_android_page.dart
+++ lib/src/components/modal_wallet_android_page.dart
@@ -12,9 +12,21 @@ class ModalWalletAndroidPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-        onPressed: () => launchUrl(Uri.parse(uri)),
-        child: const Text('Connect'),
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+                'Touch on Connect button and choose the wallet application you want to use',
+                textAlign: TextAlign.center),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () => launchUrl(Uri.parse(uri)),
+              child: const Text('Connect'),
+            ),
+          ],
+        ),
       ),
     );
   }
